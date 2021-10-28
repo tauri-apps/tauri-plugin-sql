74979bd33a1d584bd1194e7ad40a739de1c259a3
Jonas Kruckenberg <iterpre@protonmail.com>
2021-10-28 16:09:58 +0200

    provide minified and unminified esm builds

--- webview-src/rollup.config.js
+++ webview-src/rollup.config.js
@@ -1,21 +1,35 @@
-import { nodeResolve } from '@rollup/plugin-node-resolve'
-import { terser } from 'rollup-plugin-terser'
-import typescript from '@rollup/plugin-typescript'
+import resolve from "@rollup/plugin-node-resolve";
+import { terser } from "rollup-plugin-terser";
+import typescript from "@rollup/plugin-typescript";
 
-export default {
-	input: './webview-src/index.ts',
-	output: {
-		dir: './webview-dist',
-		entryFileNames: '[name].js',
-		format: 'es',
-		exports: 'auto'
-	},
-	plugins: [
-    nodeResolve(),
-		terser(),
-		typescript({
-			tsconfig: './webview-src/tsconfig.json',
-			moduleResolution: 'node'
-		})
-	]
-}
+export default [
+  {
+    input: "./webview-src/index.ts",
+    output: {
+      dir: "./webview-dist",
+      entryFileNames: "[name].min.js",
+      format: "esm",
+    },
+    plugins: [
+      resolve(),
+      terser(),
+      typescript({
+        tsconfig: "./webview-src/tsconfig.json",
+      }),
+    ],
+  },
+  {
+    input: "./webview-src/index.ts",
+    external: ['@tauri-apps/api/tauri'],
+    output: {
+      dir: "./webview-dist",
+      entryFileNames: "[name].mjs",
+      format: "esm",
+    },
+    plugins: [
+      typescript({
+        tsconfig: "./webview-src/tsconfig.json",
+      }),
+    ],
+  },
+];
