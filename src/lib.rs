1b6c5a4f8594947a333ccd2f95b0da8fb44983b7
Jonas Kruckenberg <iterpre@protonmail.com>
2022-01-20 12:00:22 +0100

    add license headers + checks

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+// Copyright 2021 Tauri Programme within The Commons Conservancy
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: MIT
+
 #[cfg(any(
     all(feature = "sqlite", feature = "mysql"),
     all(feature = "sqlite", feature = "postgres"),
--- src/plugin.rs
+++ src/plugin.rs
@@ -1,3 +1,7 @@
+// Copyright 2021 Tauri Programme within The Commons Conservancy
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: MIT
+
 use futures::future::BoxFuture;
 use serde::{ser::Serializer, Deserialize, Serialize};
 use serde_json::Value as JsonValue;
