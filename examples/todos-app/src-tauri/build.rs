4fe75ec5e6c79a82e1a3c77a0a2fbafecda555df
Fractal-Tess <vgfractal@gmail.com>
2022-11-16 12:39:01 +0200

    remove old svelte example

--- examples/todos-app/src-tauri/build.rs
+++
@@ -1,4 +0,0 @@
-fn main() {
-  println!("cargo:rustc-env=MACOSX_DEPLOYMENT_TARGET=10.13");
-  tauri_build::build()
-}
--- examples/todos-app/src/components/icons/TrashIcon.svelte
+++
@@ -1,7 +0,0 @@
-<svg class="w-full h-full fill-current stroke-current {$$props.class}" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" stroke-width="4">
-  <g fill="none" stroke-linecap="square" stroke-miterlimit="10">
-    <path data-color="color-2" d="M22.5 13.5v-7a3 3 0 013-3h14a3 3 0 013 3v7M60.5 13.5h-56" />
-    <path d="M53.5 19.5l-2.668 37.356a5 5 0 01-4.988 4.644H19.156a5 5 0
-      01-4.988-4.644L11.5 19.5" />
-  </g>
-</svg>
