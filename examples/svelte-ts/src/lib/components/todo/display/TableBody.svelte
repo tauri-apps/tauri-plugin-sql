bb6f89d4c96a68661bffe310eaf8093f21786f56
Fractal-Tess <vgfractal@gmail.com>
2022-11-18 00:33:39 +0200

    fix checkbox err, abstracted away todo component

--- examples/svelte-ts/src/lib/components/todo/display/TableBody.svelte
+++ examples/svelte-ts/src/lib/components/todo/display/TableBody.svelte
@@ -1,37 +1,10 @@
 <script lang="ts">
   import { todoStore } from '../../../stores/todoStore';
+  import Todo from './Todo.svelte';
 </script>
 
 <tbody class="text-center">
-  {#each $todoStore as { completed, id, title } (id)}
-    <tr>
-      <td>{id}</td>
-      <td class="font-bold text-lg">{title}</td>
-      <td>
-        <!-- IMPORTANTR -->
-        <!-- Ordering of the directives (bind & on:change) matters! -->
-        <input
-          bind:checked={completed}
-          on:change={() => {
-            todoStore.update({
-              completed,
-              id,
-              title,
-            });
-          }}
-          type="checkbox"
-          class="checkbox checkbox-primary"
-        />
-      </td>
-      <td>
-        <button
-          on:click={() => {
-            todoStore.remove(id);
-          }}
-          type="checkbox"
-          class="btn btn-sm btn-primary">Delete</button
-        >
-      </td>
-    </tr>
+  {#each $todoStore as todo (todo.id)}
+    <Todo {...todo} />
   {/each}
 </tbody>
