71839a01f27f6697e92d06f3d72ed726c5a12c0f
FabianLars <fabianlars@fabianlars.de>
2023-05-03 07:04:42 +0000

    Merge branch 'dev' into next. Rename next to v2
    Committed via a GitHub action: https://github.com/tauri-apps/plugins-workspace/actions/runs/4869196953
    
    Co-authored-by: FabianLars <FabianLars@users.noreply.github.com>

---
+++ src/lib.rs
@@ -0,0 +1,19 @@
+// Copyright 2021 Tauri Programme within The Commons Conservancy
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: MIT
+
+#[cfg(any(
+    all(feature = "sqlite", feature = "mysql"),
+    all(feature = "sqlite", feature = "postgres"),
+    all(feature = "mysql", feature = "postgres")
+))]
+compile_error!("Only one database driver can be enabled. Use `default-features = false` and set the feature flag for the driver of your choice.");
+
+#[cfg(not(any(feature = "sqlite", feature = "mysql", feature = "postgres")))]
+compile_error!(
+    "Database driver not defined. Please set the feature flag for the driver of your choice."
+);
+
+mod decode;
+mod plugin;
+pub use plugin::*;
